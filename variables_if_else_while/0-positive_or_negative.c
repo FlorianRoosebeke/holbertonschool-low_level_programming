1fe20a5fca38c614bf60d11f4d43d3f72a29f3ae
Florian <12160@holbertonstudents.com>
2025-10-20 15:06:41 +0200

    fix : syntax error

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: assign a random number to the variable
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -12,10 +16,10 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-	printf("%d is positive", n);
+	printf("%d is positive\n", n);
 	else if (n == 0)
-	printf("%d is zero", n);
+	printf("%d is zero\n", n);
 	else
-	printf("%d is negative", n);
+	printf("%d is negative\n", n);
 return (0);
 }
