7da0a85f1ba27a2badf224b99894237234779019
Florian <12160@holbertonstudents.com>
2025-10-21 15:29:18 +0200

    fix : correction code

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: assign a random number to the variable
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ascii;
+
+	ascii = 123;
+
+	while (ascii != 97)
+{
+		ascii--;
+		putchar(ascii);
+}
+		putchar('\n');
+		return (0);
+	}
