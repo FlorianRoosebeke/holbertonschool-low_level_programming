52f6485a18489898b89c0a421644776e20a6e093
Florian <12160@holbertonstudents.com>
2025-10-21 13:49:26 +0200

    feat : program that prints the alphabet in lowercase without e, q

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: assign a random number to the variable
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ascii;
+
+	ascii = 96;
+
+	while (ascii != 123)
+{
+		ascii++;
+		if (ascii == 101)
+			continue;
+		if (ascii == 113)
+			continue;
+		putchar(ascii);
+}
+		return (0);
+	}
