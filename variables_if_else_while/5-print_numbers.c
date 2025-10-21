c3c6f4f659c12589f159d8c5d0e6d05450b00273
Florian <12160@holbertonstudents.com>
2025-10-21 15:03:12 +0200

    feat : prints all single digit numbers of base 10 starting from 0

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: assign a random number to the variable
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int digit = -1;
+
+while (digit != 9)
+{
+digit++;
+printf("%d", digit);
+}
+return (0);
+}
