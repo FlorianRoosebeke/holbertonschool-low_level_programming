3c23e5bf1abb82091d0554daf3e9dcceafbed0c7
Florian <12160@holbertonstudents.com>
2025-10-21 10:39:00 +0200

    feat : program that prints the alphabet in lowercase

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: assign a random number to the variable
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char cha;
+
+	for (cha = 'a' ; cha <= 'z' ; cha++)
+
+		putchar(cha);
+	putchar('\n');
+	return (0);
+}
