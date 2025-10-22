732a25469595670acb7c5a0e1e6139051939bf83
Florian <12160@holbertonstudents.com>
2025-10-22 14:21:42 +0200

    fix : test

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -4,12 +4,11 @@
 *
 *Return: Always 0.
 */
-int main(void)
+void print_alphabet(void)
 {
-char cha;
-	for (cha = 'a' ; cha <= 'z' ; cha++)
-		_putchar(cha);
-	
-	_putchar('\n');
-	return (0);
+	char cha;
+for (cha ='a' ; cha <= 'z' ; cha++)
+	_putchar(cha);
+_putchar('\n');
 }
+ 
