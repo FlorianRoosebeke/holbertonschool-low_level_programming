86825866a1f9fb70a68d1b9fe532ea55b06efc24
Florian <12160@holbertonstudents.com>
2025-10-22 09:29:44 +0200

    feat : prints all possible combinations of single-digit numbers.

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: assign a random number to the variable
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+char number;
+number = 47;
+
+while (number != 57)
+{
+number++;
+putchar(number);
+putchar(44);
+putchar(32);
+}
+putchar('\n');
+return (0);
+}
