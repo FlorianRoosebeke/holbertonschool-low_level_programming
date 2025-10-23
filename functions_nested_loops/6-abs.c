80d30781cbe89dea86409dd99a22cc2cf13d160a
Florian <12160@holbertonstudents.com>
2025-10-23 13:50:39 +0200

    feat : Write a function that computes the absolute value

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * int _abs(int); _abs -Write a fonction print absolut digit.
+ * @x : print number
+ *
+ * Return: return 1 or 0 cool
+ */
+int _abs(int x)
+{
+	if (x < 0)
+	{
+		return (-x);
+	}
+	else if (x > 0)
+	{
+		return (x);
+	}
+	else
+{
+		return ('0');
+	}
+}
+
+
+
