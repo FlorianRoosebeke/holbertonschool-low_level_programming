6bd99e133d865c8fd0de9784cdccc1eb26618109
Florian <12160@holbertonstudents.com>
2025-10-23 11:30:09 +0200

    feat : Write a function that checks for lowercase character.

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - affiche l'alphabet en minuscules 10 fois
+ * suivi d'un retour à la ligne
+ * print_alphabet_x10 -x10
+ */
+
+int _islower(int c)
+{
+	if (c <= 'z' && c >= 'a')
+		return (1);
+	else
+		return (0);
+}
+
