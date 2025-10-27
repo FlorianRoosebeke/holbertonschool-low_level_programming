c8836a61f483cbb40d9a2252662a2bbe312f99fd
Florian <12160@holbertonstudents.com>
2025-10-27 16:46:59 +0100

    feat: implement print_square function to print a square in the terminal and update header

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - Write a function that prints a square, followed by a new line
+ * size : is the size of the square
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int i, n;
+
+	if (!(size <= 0))
+
+		for (i = 0 ; i < size ; i++)
+		{
+			for (n = 0 ; n < size ; n++)
+			{
+			_putchar('#');
+			}
+		_putchar('\n');
+
+		}
+	else
+		_putchar('\n');
+}
