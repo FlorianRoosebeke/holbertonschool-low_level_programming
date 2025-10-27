dfe648a8895c625d6f93a537ec33e1d5ffdc7b4d
Florian <12160@holbertonstudents.com>
2025-10-27 13:24:28 +0100

    fix: update documentation for print_line function to clarify parameters and return type

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
-* print_line -Write a function that draws a straight line in the terminal.
-* @n - number of times the character
-* Return: Always 0.
-*/
+ * print_line - draws a straight line in the terminal
+ * @n: number of times the character '_' should be printed
+ *
+ * Return: void
+ */
 
 void print_line(int n)
 {
