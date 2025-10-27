ec027e1efe20d5f8daa015211ed922be3eb9a9cb
Florian <12160@holbertonstudents.com>
2025-10-27 15:04:48 +0100

    feat: implement print_diagonal function to draw a diagonal line in the terminal and update header

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n:  number of times the character \ should be printed
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int colonne, ligne;
+
+	if (n != 0)
+
+		for (colonne = 0; colonne < n; colonne++)
+		{
+			for (ligne = 0; ligne < colonne; ligne++)
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	else
+		_putchar('\n');
+}
