49f2d6a359be9b04ee1e36c9e81a8b938b241d98
Florian <12160@holbertonstudents.com>
2025-10-27 10:19:00 +0100

    fix: correct formatting and add missing newline in _isupper function

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -2,21 +2,21 @@
 #include <stdio.h>
 
 /**
- * main - check the code.
- *
+ * _isupper -Check if uppercase or other
+ * @c : checker
  * Return: Always 0.
  */
 
 int _isupper(int c)
 {
 
-	if (c >= 'A' && c <= 'Z') 
+	if (c >= 'A' && c <= 'Z')
 	{
-	return 1;
+	return (1);
 	}
-	else 
+	else
 	{
-	return 0;	
+	return (0);
 	}
 
 }
