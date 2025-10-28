9f436c6d7d05147c0a402967f2f9eae48ca3a1fa
Florian <12160@holbertonstudents.com>
2025-10-28 09:31:29 +0100

    feat: implement Fizz-Buzz program to print numbers from 1 to 100 with conditions

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - The “Fizz-Buzz test” program that prints numbers from 1 to 100.
+ *
+ * Description: For multiples of three, print "Fizz" instead of the number,
+ * for multiples of five print "Buzz", and for numbers which are multiples
+ * of both three and five print "FizzBuzz".
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int nbre;
+
+	for (nbre = 1; nbre <= 100; nbre++)
+	{
+		if (nbre % 3 == 0 && nbre % 5 == 0)
+		{
+			printf("FizBuzz ");
+		}
+		else if (nbre % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (nbre % 5 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", nbre);
+		}
+	}
+	printf("\n");
+	return (0);
+}
