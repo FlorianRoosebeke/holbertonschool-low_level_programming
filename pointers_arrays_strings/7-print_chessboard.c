987075e52b6b33d3cedde5eb8860517dc51a0faf
Florian <12160@holbertonstudents.com>
2025-11-03 21:27:33 +0100

    fix: correct logic in _strstr function to properly locate a substring feat: update print_chessboard function to improve formatting and clarity

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -2,26 +2,22 @@
 #include <stddef.h>
 #include <stdio.h>
 /**
- * _strstr - Locates a substring.
- * @haystack: String to be searched
- * @needle: Substring to look for
- * Return: Pointer to the beginning of the located substring,
- *         or NULL if the substring is not found
+ * print_chessboard - Prints the chessboard.
+ * @a: 2D array representing the chessboard
+ * Return: Nothing
  */
 
 void print_chessboard(char (*a)[8])
 {
 
-int i, j;
+	int i, j;
 
-for (i = 0 ; i < 8 ; i++)
-{
-	for (j = 0 ; j < 8 ; j++)
+	for (i = 0; i < 8; i++)
 	{
-	 printf("a[%d][%d] =  %d\n", i, j, a[i][j]);
+		for (j = 0; j < 8; j++)
+		{
+			printf("%c", a[i][j]);
+		}
+		putchar('\n');
 	}
-	putchar('\n');
-}
-
-
 }
