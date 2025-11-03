6cbade9873bc787647f9858afc58173ed3f5d0cb
Florian <12160@holbertonstudents.com>
2025-11-03 12:32:39 +0100

    feat: implement _strspn function to calculate the length of a prefix substring and declare its prototype in main.h

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to search
+ * @accept: bytes to accept
+ * Return: number of bytes in the initial segment of s
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		for (j = 0 ; accept[j] != '\0' ; j++)
+		{
+			if (s[i] == accept[j])
+			break;
+
+		}
+	if (accept[j] == '\0')
+		return (i);
+	}
+return (i);
+}
+
