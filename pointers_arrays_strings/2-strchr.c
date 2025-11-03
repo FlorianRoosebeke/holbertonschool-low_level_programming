a0d7a3b940437c70140d3a4e594d93cbccaeeed0
Florian <12160@holbertonstudents.com>
2025-11-03 10:30:00 +0100

    feat: implement _strchr function to locate a character in a string and declare its prototype in main.h

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to search
+ * @c: character to find
+ * Return: pointer to first occurrence of c in s, or NULL if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+int i = 0;
+
+while (s[i] != '\0')
+{
+if (s[i] == c)
+	return (&s[i]);
+i++;
+}
+return (NULL);
+
+}
