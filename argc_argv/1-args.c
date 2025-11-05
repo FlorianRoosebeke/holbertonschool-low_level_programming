38ab338094d843a7b13d58c738d06f80c7cb00cb
Florian <12160@holbertonstudents.com>
2025-11-05 15:23:31 +0100

    refactor: simplify main function to print the number of command line arguments

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -10,14 +10,10 @@
 
 int main(int argc, char **argv)
 {
-	int i;
 	(void)argc;
 	(void)argv;
 
-for (i = 0 ; i < argc ; i++)
-	{
-	  printf("%d\n", argc);
-	}
-_putchar('\n');
+	printf("%d\n", argc - 1);
+
 return (0);
 }
