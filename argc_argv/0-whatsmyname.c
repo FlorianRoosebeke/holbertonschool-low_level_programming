2238bffe60d7fbe3176b264aa3e7b0bc6711c450
Florian <12160@holbertonstudents.com>
2025-11-05 14:13:16 +0100

    feat: implement main function to print the program name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Prints the program name.
+ * @argc: Number of command-line arguments (unused).
+ * @argv: Array of command-line argument strings.
+ *
+ * Return: 0 on success.
+ */
+int main(int argc, char **argv)
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
