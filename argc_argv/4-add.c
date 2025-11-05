fda788e5a0ca4567131228053ab1de05bcd19baf
Florian <12160@holbertonstudents.com>
2025-11-05 20:56:20 +0100

    feat: implement addition of command line arguments

---
+++ argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the number of command line arguments
+ * @argc: number of arguments passed to the program
+ * @argv: array of argument strings
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char **argv)
+{
+	int i, sum = 0;
+	(void)argc;
+	(void)argv;
+
+	{
+		for (i = 1 ; i < argc ; i++)
+		{
+		sum += atoi(argv[i]);
+		}
+		printf("%d\n", sum);
+	}
+
+		if (argc <= 1)
+	{
+		putchar('\n');
+	}
+
+	return (0);
+}
