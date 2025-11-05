c5e38b529164df1d20422532dd0b1559ca761c28
Florian <12160@holbertonstudents.com>
2025-11-05 22:38:54 +0100

    fix: include ctype.h for isdigit function and improve input validation in addition program

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <ctype.h>
 /**
  * main - prints the number of command line arguments
  * @argc: number of arguments passed to the program
@@ -9,12 +9,9 @@
  * Return: 0 on success
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-
 int main(int argc, char **argv)
 {
-	int i, sum = 0;
+	int i, j, sum = 0;
 
 	if (argc <= 1)
 	{
@@ -22,14 +19,19 @@ int main(int argc, char **argv)
 		return (1);
 	}
 
-
 	for (i = 1; i < argc; i++)
 	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
 		sum += atoi(argv[i]);
 	}
-
 	printf("%d\n", sum);
-
-
 	return (0);
-}
+	}
