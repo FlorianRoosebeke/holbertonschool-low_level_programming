83860ba422c1433bd4ac5f0c34bc70bb73d4fbc4
Florian <12160@holbertonstudents.com>
2025-11-05 15:16:06 +0100

    fix: correct formatting and include main.h in 1-args.c

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include"main.h"
 /**
  * main - prints the number of command line arguments
  * @argc: number of arguments passed to the program
@@ -9,13 +10,14 @@
 
 int main(int argc, char **argv)
 {
-
+	int i;
 	(void)argc;
 	(void)argv;
 
-	
+for (i = 0 ; i < argc ; i++)
 	{
 	  printf("%d\n", argc);
 	}
+_putchar('\n');
 return (0);
 }
