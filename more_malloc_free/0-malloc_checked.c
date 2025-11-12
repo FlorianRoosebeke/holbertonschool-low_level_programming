fce2f62821cf34597561d8c4d11f77d7d7a2d550
Florian <12160@holbertonstudents.com>
2025-11-12 10:57:03 +0100

    feat: implement malloc_checked function to allocate memory and handle allocation failure

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory using malloc and exits on failure
+ * @b: number of bytes to allocate
+ *
+ * Return: pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+
+	return (ptr);
+}
