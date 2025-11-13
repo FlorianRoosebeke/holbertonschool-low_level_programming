9fe5c8cbfde18b6c0804a05c0c7613a3c7e1c6b3
Florian <12160@holbertonstudents.com>
2025-11-13 10:57:40 +0100

    feat: add dog structure definition in dog.h and README.md

---
+++ structures_typedef/README.md
@@ -0,0 +1,4 @@
+ struct dog - Describes a dog.
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Owner of the dog.
---
+++ structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+
+/**
+ * struct dog - Describes a dog.
+ * @name: Name of the dog.
+ * @age: Age of the dog.
+ * @owner: Owner of the dog.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
+
+
+
