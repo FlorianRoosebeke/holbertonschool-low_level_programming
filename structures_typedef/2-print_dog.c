d5aa552cd6465b8b0a1378b6720556bd698ff98e
Florian <12160@holbertonstudents.com>
2025-11-14 10:00:14 +0100

    fix: update print_dog function to include labels for nil values in name and owner fields

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -17,7 +17,7 @@ void print_dog(struct dog *d)
 	if (d->name != NULL)
 	printf("Name: %s\n", d->name);
 	else
-	printf("(nil)\n");
+	printf("Name: (nil)\n");
 	if (d->age)
 	printf("Age: %f\n", d->age);
 	else
@@ -25,7 +25,7 @@ void print_dog(struct dog *d)
 	if (d->owner != NULL)
 	printf("Owner: %s\n", d->owner);
 	else
-	printf("(nil)\n");
+	printf("Owner: (nil)\n");
 }
 
 
