6b6be10ff3e692789afa7f151c41e556a01a50da
Florian <12160@holbertonstudents.com>
2025-11-17 15:48:10 +0100

    fix: update print_numbers function to correct documentation and improve separator handling

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -4,16 +4,19 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - Returns the sum of all its integer parameters
- * @n: The number of integer arguments passed to the function
+ * print_numbers - Prints integers passed variadic arguments followed new line
+ * @separator: string to be printed between numbers (can be NULL)
+ * @n: the number of integers passed to the function
  *
- * Return: If n is 0 returns 0 otherwise returns the sum the provided integers.
+ * Description: Prints the n integers supplied as variadic arguments separated
+ * by the string separator; if separator is NULL the numbers are printed
+ * without any separator. A trailing newline is printed after the numbers.
  */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i = 0;
-	int nbre;
+	unsigned int nbre;
 	va_list ap;
 
 	va_start(ap, n);
@@ -21,11 +24,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		nbre = va_arg(ap, int);
-	printf("%d", nbre);
+		printf("%d", nbre);
 
-	if (nbre != 402)
-		printf("%s", separator);
-
+		if (i != n - 1)
+			printf("%s", separator);
 	}
-putchar('\n');
+	putchar('\n');
 }
