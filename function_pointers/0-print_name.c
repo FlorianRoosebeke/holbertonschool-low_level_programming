d793332bef3917a6d6148ffc452094aadd058d77
Florian <12160@holbertonstudents.com>
2025-11-17 10:20:57 +0100

    feat: implement print_name and _putchar functions with corresponding header and documentation

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - call a function to print a name
+ * @name: the name to be printed
+ * @f: pointer to a function that takes a char * and returns void
+ *
+ * Description: If name or f is NULL the function does nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+	f(name);
+}
