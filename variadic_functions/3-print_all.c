ac1283c6962a1a18035c4a9f8344bfe8bd0ddb9d
Florian <12160@holbertonstudents.com>
2025-11-20 14:25:00 +0100

    fix: remove unnecessary break statement in print_all function for string handling

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -44,7 +44,6 @@ void print_all(const char *const format, ...)
 			pr_string = va_arg(ap, char *);
 			if (pr_string == NULL)
 				printf("(nil)");
-				break;
 			{
 				printf("%s", pr_string);
 				break;
