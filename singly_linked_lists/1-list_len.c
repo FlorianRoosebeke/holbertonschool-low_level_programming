ab7c4bfe6e54b7c3193710439c84886c661784fe
Florian <12160@holbertonstudents.com>
2025-12-01 11:17:45 +0100

    feat: add list_len function to count elements in a list_t list

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a list_t list
+ * @h: pointer to the list to count
+ *
+ * Return: the number of nodes
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
