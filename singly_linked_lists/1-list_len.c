ed7e12417c5a4734dd16c5c1ba59aaf028f0b9a7
Florian <12160@holbertonstudents.com>
2025-12-01 11:18:57 +0100

    fix: correct format in list_len function output

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -18,7 +18,7 @@ size_t list_len(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%u]\n", h->len, h->str);
 
 		h = h->next;
 		count++;
