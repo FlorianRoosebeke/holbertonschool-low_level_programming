f7210d5a0a430513e588de4c5ccd3c9ff2c36509
Florian <12160@holbertonstudents.com>
2025-12-02 11:14:20 +0100

    fix: format free_list function for consistency and readability

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -9,17 +9,17 @@
  *
  * Return: void
  */
-void free_list(list_t *head);
+void free_list(list_t *head)
 {
 
 	list_t *temp;
 
 	while (head != NULL)
 	{
-	temp = head->next;
-	free(head->str);
-	free(head);
-	head = temp;
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
 	}
-
 }
+
