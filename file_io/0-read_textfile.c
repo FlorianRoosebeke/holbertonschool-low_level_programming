57385a09f94b0aa844864cc7f5b4463576f4b24e
Florian <12160@holbertonstudents.com>
2025-12-04 11:15:46 +0100

    fix: handle write errors in read_textfile function

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -34,6 +34,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	count = write(1, buffer, count);
+	if (count == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 	free(buffer);
 
 	close(fd);
